3db53624ac9c5ba6a8e040369d6a53705900c8a8
Ryan Doherty <tech@conical.org>
2022-05-18 15:41:10 -0400

    Show how other features already included in dependencies can be used

--- src/main/java/org/veupathdb/toolsample/Main.java
+++ src/main/java/org/veupathdb/toolsample/Main.java
@@ -2,12 +2,18 @@ package org.veupathdb.toolsample;
 
 import static org.gusdb.fgputil.runtime.Environment.getRequiredVar;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.gusdb.fgputil.db.platform.SupportedPlatform;
 import org.gusdb.fgputil.db.pool.DatabaseInstance;
 import org.gusdb.fgputil.db.pool.SimpleDbConfig;
 import org.gusdb.fgputil.db.runner.SQLRunner;
+import org.gusdb.fgputil.db.slowquery.QueryLogConfig;
+import org.gusdb.fgputil.db.slowquery.QueryLogger;
 
 public class Main {
+
+  public static final Logger LOG = LogManager.getLogger(Main.class);
 
   public static void main(String args[]) throws Exception {
 
@@ -16,6 +22,9 @@ public class Main {
     String connectionUser = getRequiredVar("DB_USERNAME");
     String connectionPassword = getRequiredVar("DB_PASSWORD");
 
+    // initialize query logger with default config
+    QueryLogger.initialize(new QueryLogConfig(){});
+
     // instantiate a connection to the database
     try (DatabaseInstance db = new DatabaseInstance(SimpleDbConfig.create(
         SupportedPlatform.ORACLE, connectionUrl, connectionUser, connectionPassword))) {
@@ -23,7 +32,7 @@ public class Main {
       // do some db work here
       new SQLRunner(db.getDataSource(), "select 1 from dual").executeQuery(rs -> null);
 
-      System.out.println("Done.");
+      LOG.info("Done.");
     }
   }
 }
